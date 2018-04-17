300c3ed4a4a1337c00b7ee804099e2fa03090248
patrick brisbin <pbrisbin@gmail.com>
2018-04-17 11:41:07 -0400

    Ensure 100% coverage for an empty source file
    The choice is arbitrary, so we'll go with this one since it avoids a 0%
    file showing up in file listings and causing anxiety.
    
    Note that the repo-wide coverage is always calculated as total coverage
    over total lines, so empty files won't impact things either way.

--- src/CC/Coverage/Percentage.hs
+++ src/CC/Coverage/Percentage.hs
@@ -10,8 +10,14 @@ import Data.Aeson
 newtype Percentage = Percentage Float
     deriving ToJSON
 
+-- | Safe constructor
+--
+-- - Restricts values to @[0, 100]@
+-- - Returns 100% for a 0 denominator (this is an arbitrary choice)
+--
 toPercentage :: (Integral a, Integral b) => a -> b -> Percentage
 toPercentage n d
+    | d == 0 = Percentage 100
     | f < 0 = Percentage 0
     | f > 100 = Percentage 100
     | otherwise = Percentage f
